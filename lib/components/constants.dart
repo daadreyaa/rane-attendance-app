d052bcbf1b85c3373d6172c1b16d115aeea90433
Daadreyaa <daadreyaa@gmail.com>
2022-07-08 23:57:36 +0530

    homescreen completed as per requirement

--- lib/components/constants.dart
+++ lib/components/constants.dart
@@ -5,4 +5,5 @@ const Color kTertiaryColor = Color(0xFFF1F4F8);
 const Color kSecondaryColor = Color(0xFF6C787F);
 const Color kPrimaryColor = Color(0xFF0F1113);
 
- TextStyle kPrimaryText =  GoogleFonts.lexendDeca(fontSize: 20,fontWeight: FontWeight.w500);
+TextStyle kPrimaryText = GoogleFonts.lexendDeca(fontSize: 20, fontWeight: FontWeight.w500);
+TextStyle kSecondaryText = GoogleFonts.lexendDeca(fontSize: 14, fontWeight: FontWeight.w400);
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:rane_mobile_app/components/constants.dart';
 import 'package:rane_mobile_app/screens/attendance_screen.dart';
 import 'package:rane_mobile_app/screens/forgot_punch.dart';
 import 'package:rane_mobile_app/screens/leave_screen.dart';
+import 'package:rane_mobile_app/screens/ovetime_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -21,54 +23,117 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text(
           'Welcome',
           style: GoogleFonts.lexendDeca(
-            textStyle: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.w700),
+            textStyle: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.w700, color: Colors.black),
           ),
         ),
-        backgroundColor: Colors.grey.shade800,
+        backgroundColor: const Color(0xffe4e3e9),
       ),
+      backgroundColor: const Color(0xffe4e3e9),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            CircleAvatar(
-              child: Image.network(
-                'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Rajalakshmi_Engineering_College_%28REC%29_Chennai_Logo.jpg/626px-Rajalakshmi_Engineering_College_%28REC%29_Chennai_Logo.jpg',
+            const SizedBox(
+              height: 15,
+            ),
+            Container(
+              alignment: Alignment.center,
+              height: 180,
+              width: 180,
+              child: const CircleAvatar(
+                backgroundImage: NetworkImage(
+                  'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Rajalakshmi_Engineering_College_%28REC%29_Chennai_Logo.jpg/626px-Rajalakshmi_Engineering_College_%28REC%29_Chennai_Logo.jpg',
+                ),
+                radius: 80,
               ),
-              radius: 100,
-              backgroundColor: Colors.white,
+              decoration: const BoxDecoration(
+                color: Color(0xffefeeee),
+                shape: BoxShape.circle,
+                // borderRadius: BorderRadius.circular(10),
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Color(0xffefeeee),
+                    Color(0xffefeeee),
+                  ],
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0xffffffff),
+                    offset: Offset(-20.0, -20.0),
+                    blurRadius: 30,
+                    spreadRadius: 0.0,
+                  ),
+                  BoxShadow(
+                    color: Color(0xffd1d0d0),
+                    offset: Offset(20.0, 20.0),
+                    blurRadius: 30,
+                    spreadRadius: 0.0,
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Divider(
+              thickness: 1.5,
+              indent: 30,
+              endIndent: 30,
+            ),
+            const SizedBox(
+              height: 10,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                SelectOption(
-                  option: 'Dashboard',
+                SelectOptionContainer(
+                  title: 'Dashboard',
+                  imageLocation: 'images/dashboard.png',
                   onTap: () {},
                 ),
-                SelectOption(
-                  option: 'Attendance',
+                SelectOptionContainer(
+                  title: 'Attendance',
+                  imageLocation: 'images/attendance.png',
                   onTap: () => Navigator.pushNamed(context, AttendanceScreen.id),
-
-                  // onTap: () => print('clicked'),
                 ),
               ],
             ),
+            const SizedBox(
+              height: 20,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                SelectOption(
-                  option: 'Leave/OD',
+                SelectOptionContainer(
+                  title: 'Leave/OD',
+                  imageLocation: 'images/leave.png',
                   onTap: () => Navigator.pushNamed(context, LeavePage.id),
                 ),
-                SelectOption(
-                  option: 'Shift Change',
+                SelectOptionContainer(
+                  title: 'Shift Change',
+                  imageLocation: 'images/change_shift.png',
                   onTap: () {},
                 ),
               ],
             ),
-            SelectOption(
-              option: 'Forgot Punch',
-              onTap: () => Navigator.pushNamed(context, ForgotPunch.id),
-              width: 250,
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                SelectOptionContainer(
+                  title: 'Overtime',
+                  imageLocation: 'images/overtime.png',
+                  onTap: () => Navigator.pushNamed(context, OvertimeScreen.id),
+                ),
+                SelectOptionContainer(
+                  title: 'Forgot Punch',
+                  imageLocation: 'images/forgot_punch.png',
+                  onTap: () => Navigator.pushNamed(context, ForgotPunch.id),
+                ),
+              ],
             ),
           ],
         ),
@@ -77,39 +142,65 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
-class SelectOption extends StatelessWidget {
-  // const SelectOption({
+class SelectOptionContainer extends StatelessWidget {
+  // const SelectOptionContainer({
   //   Key? key,
   // }) : super(key: key);
 
-  const SelectOption({required this.option, required this.onTap, this.width = 175});
+  SelectOptionContainer({required this.title, required this.imageLocation, required this.onTap});
 
-  final String option;
+  final String title;
+  final String imageLocation;
   final Function()? onTap;
-  final double width;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: onTap,
       child: Container(
-        child: Center(
-          child: Text(
-            option,
-            style: GoogleFonts.lexendDeca(
-              textStyle: const TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.w500,
-              ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image.asset(
+              imageLocation,
+              height: 50,
+              width: 50,
             ),
-          ),
+            Text(
+              title,
+              style: kSecondaryText,
+            ),
+          ],
         ),
+        height: 110,
+        width: 110,
+        alignment: Alignment.center,
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10.0),
-          color: Colors.indigo.shade100,
+          color: const Color(0xffefeeee),
+          borderRadius: BorderRadius.circular(10),
+          gradient: const LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color(0xffefeeee),
+              Color(0xffefeeee),
+            ],
+          ),
+          boxShadow: const [
+            BoxShadow(
+              color: Color(0xffffffff),
+              offset: Offset(-15.0, -15.0),
+              blurRadius: 30,
+              spreadRadius: 0.0,
+            ),
+            BoxShadow(
+              color: Color(0xffd1d0d0),
+              offset: Offset(15.0, 15.0),
+              blurRadius: 30,
+              spreadRadius: 0.0,
+            ),
+          ],
         ),
-        height: 60,
-        width: width,
       ),
     );
   }
