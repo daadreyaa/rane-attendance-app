1364eaf73ea684764279d9d04a8ad890ada828a7
Akhash16 <akhash.xs@gmail.com>
2022-07-20 17:25:20 +0530

    calendar finished

--- lib/screens/otp_screen.dart
+++ lib/screens/otp_screen.dart
@@ -4,11 +4,10 @@ import 'dart:async';
 
 import 'package:flutter_verification_code/flutter_verification_code.dart';
 import 'package:rane_mobile_app/components/constants.dart';
-
-
+import 'package:rane_mobile_app/screens/home_screen.dart';
 
 class OTPPage extends StatefulWidget {
-  const OTPPage({ Key? key }) : super(key: key);
+  const OTPPage({Key? key}) : super(key: key);
   static String id = "otp_screen";
 
   @override
@@ -32,7 +31,7 @@ class _OTPPageState extends State<OTPPage> {
     });
 
     const oneSec = Duration(seconds: 1);
-    _timer = new Timer.periodic(oneSec, (timer) { 
+    _timer = new Timer.periodic(oneSec, (timer) {
       setState(() {
         if (_start == 0) {
           _start = 60;
@@ -51,7 +50,7 @@ class _OTPPageState extends State<OTPPage> {
     });
 
     const oneSec = Duration(milliseconds: 2000);
-    _timer = new Timer.periodic(oneSec, (timer) { 
+    _timer = new Timer.periodic(oneSec, (timer) {
       setState(() {
         _isLoading = false;
         _isVerified = true;
@@ -65,8 +64,7 @@ class _OTPPageState extends State<OTPPage> {
       setState(() {
         _currentIndex++;
 
-        if (_currentIndex == 3)
-          _currentIndex = 0;
+        if (_currentIndex == 3) _currentIndex = 0;
       });
     });
 
@@ -76,135 +74,186 @@ class _OTPPageState extends State<OTPPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 20),
-          height: MediaQuery.of(context).size.height,
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                height: 250,
-                child: Stack(
-                  children: [
-                    Positioned(
-                      top: 0,
-                      left: 0,
-                      right: 0,
-                      bottom: 0,
-                      child: AnimatedOpacity(
-                        opacity: _currentIndex == 0 ? 1 : 0, 
-                        duration: Duration(seconds: 1,),
-                        curve: Curves.linear,
-                        child: Image.network('https://ouch-cdn2.icons8.com/eza3-Rq5rqbcGs4EkHTolm43ZXQPGH_R4GugNLGJzuo/rs:fit:784:784/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvNjk3/L2YzMDAzMWUzLTcz/MjYtNDg0ZS05MzA3/LTNkYmQ0ZGQ0ODhj/MS5zdmc.png',),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              height: MediaQuery.of(context).size.height,
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 250,
+                    child: Stack(children: [
+                      Positioned(
+                        top: 0,
+                        left: 0,
+                        right: 0,
+                        bottom: 0,
+                        child: AnimatedOpacity(
+                          opacity: _currentIndex == 0 ? 1 : 0,
+                          duration: Duration(
+                            seconds: 1,
+                          ),
+                          curve: Curves.linear,
+                          child: Image.network(
+                            'https://ouch-cdn2.icons8.com/eza3-Rq5rqbcGs4EkHTolm43ZXQPGH_R4GugNLGJzuo/rs:fit:784:784/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvNjk3/L2YzMDAzMWUzLTcz/MjYtNDg0ZS05MzA3/LTNkYmQ0ZGQ0ODhj/MS5zdmc.png',
+                          ),
+                        ),
                       ),
+                      Positioned(
+                        top: 0,
+                        left: 0,
+                        right: 0,
+                        bottom: 0,
+                        child: AnimatedOpacity(
+                          opacity: _currentIndex == 1 ? 1 : 0,
+                          duration: Duration(seconds: 1),
+                          curve: Curves.linear,
+                          child: Image.network(
+                            'https://ouch-cdn2.icons8.com/pi1hTsTcrgVklEBNOJe2TLKO2LhU6OlMoub6FCRCQ5M/rs:fit:784:666/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvMzAv/MzA3NzBlMGUtZTgx/YS00MTZkLWI0ZTYt/NDU1MWEzNjk4MTlh/LnN2Zw.png',
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        top: 0,
+                        left: 0,
+                        right: 0,
+                        bottom: 0,
+                        child: AnimatedOpacity(
+                          opacity: _currentIndex == 2 ? 1 : 0,
+                          duration: Duration(seconds: 1),
+                          curve: Curves.linear,
+                          child: Image.network(
+                            'https://ouch-cdn2.icons8.com/ElwUPINwMmnzk4s2_9O31AWJhH-eRHnP9z8rHUSS5JQ/rs:fit:784:784/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvNzkw/Lzg2NDVlNDllLTcx/ZDItNDM1NC04YjM5/LWI0MjZkZWI4M2Zk/MS5zdmc.png',
+                          ),
+                        ),
+                      )
+                    ]),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  FadeInDown(
+                      duration: Duration(milliseconds: 500),
+                      child: Text(
+                        "Verification",
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      )),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  FadeInDown(
+                    delay: Duration(milliseconds: 500),
+                    duration: Duration(milliseconds: 500),
+                    child: Text(
+                      "Please enter the 4 digit code sent to \n +93 706-399-999",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey.shade500,
+                          height: 1.5),
                     ),
-                    Positioned(
-                      top: 0,
-                      left: 0,
-                      right: 0,
-                      bottom: 0,
-                      child: AnimatedOpacity(
-                        opacity: _currentIndex == 1 ? 1 : 0, 
-                        duration: Duration(seconds: 1),
-                        curve: Curves.linear,
-                        child: Image.network('https://ouch-cdn2.icons8.com/pi1hTsTcrgVklEBNOJe2TLKO2LhU6OlMoub6FCRCQ5M/rs:fit:784:666/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvMzAv/MzA3NzBlMGUtZTgx/YS00MTZkLWI0ZTYt/NDU1MWEzNjk4MTlh/LnN2Zw.png',),
-                      ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+
+                  // Verification Code Input
+                  FadeInDown(
+                    delay: Duration(milliseconds: 600),
+                    duration: Duration(milliseconds: 500),
+                    child: VerificationCode(
+                      length: 4,
+                      textStyle: TextStyle(fontSize: 20, color: Colors.black),
+                      underlineColor: Colors.black,
+                      keyboardType: TextInputType.number,
+                      underlineUnfocusedColor: Colors.black,
+                      onCompleted: (value) {
+                        setState(() {
+                          _code = value;
+                        });
+                      },
+                      onEditing: (value) {},
                     ),
-                    Positioned(
-                      top: 0,
-                      left: 0,
-                      right: 0,
-                      bottom: 0,
-                      child: AnimatedOpacity(
-                        opacity: _currentIndex == 2 ? 1 : 0, 
-                        duration: Duration(seconds: 1),
-                        curve: Curves.linear,
-                        child: Image.network('https://ouch-cdn2.icons8.com/ElwUPINwMmnzk4s2_9O31AWJhH-eRHnP9z8rHUSS5JQ/rs:fit:784:784/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvNzkw/Lzg2NDVlNDllLTcx/ZDItNDM1NC04YjM5/LWI0MjZkZWI4M2Zk/MS5zdmc.png',),
-                      ),
-                    )
-                  ]
-                ),
-              ),
-              SizedBox(height: 30,),
-              FadeInDown(
-                duration: Duration(milliseconds: 500),
-                child: Text("Verification", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),)),
-              SizedBox(height: 30,),
-              FadeInDown(
-                delay: Duration(milliseconds: 500),
-                duration: Duration(milliseconds: 500),
-                child: Text("Please enter the 4 digit code sent to \n +93 706-399-999", 
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 16, color: Colors.grey.shade500, height: 1.5),),
-              ),
-              SizedBox(height: 30,),
+                  ),
 
-              // Verification Code Input
-              FadeInDown(
-                delay: Duration(milliseconds: 600),
-                duration: Duration(milliseconds: 500),
-                child: VerificationCode(
-                  length: 4,
-                  textStyle: TextStyle(fontSize: 20, color: Colors.black),
-                  underlineColor: Colors.black,
-                  keyboardType: TextInputType.number,
-                  underlineUnfocusedColor: Colors.black,
-                  onCompleted: (value) {
-                    setState(() {
-                      _code = value;
-                    });
-                  }, 
-                  onEditing: (value) {},
-                ),
-              ),
-
-
-              SizedBox(height: 20,),
-              FadeInDown(
-                delay: Duration(milliseconds: 700),
-                duration: Duration(milliseconds: 500),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Don't resive the OTP?", style: TextStyle(fontSize: 14, color: Colors.grey.shade500),),
-                    TextButton(
-                      onPressed: () {
-                        if (_isResendAgain) return;
-                        resend();
-                      }, 
-                      child: Text(_isResendAgain ? "Try again in " + _start.toString() : "Resend", style: TextStyle(color: Colors.blueAccent),)
-                    )
-                  ],
-                ),
-              ),
-              SizedBox(height: 50,),
-              FadeInDown(
-                delay: Duration(milliseconds: 800),
-                duration: Duration(milliseconds: 500),
-                child: MaterialButton(
-                  elevation: 0,
-                  onPressed: _code.length < 4 ? () => {} : () { verify(); },
-                  color: kRoyaleBlue,
-                  minWidth: MediaQuery.of(context).size.width * 0.8,
-                  height: 50,
-                  child: _isLoading ? Container(
-                    width: 20,
+                  SizedBox(
                     height: 20,
-                    child: CircularProgressIndicator(
-                      backgroundColor: Colors.white,
-                      strokeWidth: 3,
-                      color: Colors.black,
+                  ),
+                  FadeInDown(
+                    delay: Duration(milliseconds: 700),
+                    duration: Duration(milliseconds: 500),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Don't resive the OTP?",
+                          style: TextStyle(
+                              fontSize: 14, color: Colors.grey.shade500),
+                        ),
+                        TextButton(
+                            onPressed: () {
+                              if (_isResendAgain) return;
+                              resend();
+                            },
+                            child: Text(
+                              _isResendAgain
+                                  ? "Try again in " + _start.toString()
+                                  : "Resend",
+                              style: TextStyle(color: Colors.blueAccent),
+                            )),
+                      ],
                     ),
-                  ) : _isVerified ? Icon(Icons.check_circle, color: Colors.white, size: 30,) : Text("Verify", style: TextStyle(color: Colors.white,fontSize: 20),),
-                ),
-              )
-          ],)
-        ),
-      )
-    );
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  FadeInDown(
+                    delay: Duration(milliseconds: 800),
+                    duration: Duration(milliseconds: 500),
+                    child: MaterialButton(
+                      elevation: 0,
+                      onPressed: (){
+                        Navigator.pushNamed(context,HomeScreen.id);
+                      },
+                      // onPressed: _code.length < 4
+                      //     ? () => {Navigator.pushNamed(context, HomeScreen.id);}
+                      //     : () {
+                      //         // verify();
+                      //         Navigator.pushNamed(context, HomeScreen.id);
+                      //       },
+                      color: kRoyaleBlue,
+                      minWidth: MediaQuery.of(context).size.width * 0.8,
+                      height: 50,
+                      child: _isLoading
+                          ? Container(
+                              width: 20,
+                              height: 20,
+                              child: CircularProgressIndicator(
+                                backgroundColor: Colors.white,
+                                strokeWidth: 3,
+                                color: Colors.black,
+                              ),
+                            )
+                          : _isVerified
+                              ? Icon(
+                                  Icons.check_circle,
+                                  color: Colors.white,
+                                  size: 30,
+                                )
+                              : Text(
+                                  "Verify",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 20),
+                                ),
+                    ),
+                  )
+                ],
+              )),
+        ));
   }
 }
